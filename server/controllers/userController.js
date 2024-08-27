d2839f4d39599d01da42c5d7d7372721f9ba3ee3
Scholarbee <scholarbee6@gmail.com>
2024-08-27 02:47:29 +0000

    added getUsers function and route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -111,3 +111,11 @@ exports.getUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid user data");
   }
 });
+
+/**
+ * Get all users
+ */
+exports.getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.status(200).json({ success: true, users });
+});
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userAuth = require("../middleWare/authMiddleware");
-const { addtUser, getUser } = require("../controllers/userController");
+const { addtUser, getUser, getUsers } = require("../controllers/userController");
 const Multer = require("multer");
 
 const storage = new Multer.memoryStorage();
@@ -8,6 +8,7 @@ const upload = Multer({
   storage,
 });
 
+router.get("/", userAuth, getUsers);
 router.post("/add-user", upload.single("my_file"), addtUser);
 router.get("/user", userAuth, getUser);
 
